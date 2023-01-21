5e9e1a222890244d29c9ee0739e14e0e3ad1468f
MeiNanziiii <oplotDev@gmail.com>
2023-01-21 16:26:11 +0200

    Updated structure && localization

--- bin/index.js
+++ bin/index.js
@@ -1,36 +1,45 @@
 #!/usr/bin/env node
 
 // packages
-const kleur = require("kleur")
 const yargs = require("yargs")
 
+// config
+const config = require("../config/config.json")
+
+// lib/commands
+const init = require("../lib/commands/init.js")
+const lang = require("../lib/commands/lang.js")
+const run = require("../lib/commands/run.js")
+const update = require("../lib/commands/update.js")
+
 // lib
-const init = require("../lib/init.js")
-const run = require("../lib/run.js")
-const lang = require("../lib/lang.js")
-const update = require("../lib/update.js")
-const translate = require("../lib/translate.js")
-const config = require("../lib/config.json")
+const locale = require("../lib/locale.js")
 
 const options = yargs
     .locale(config.lang)
-    .command("init", translate.initDescription())
-    .command("run", translate.runDescription())
-    .command("lang", translate.langDescription())
-    .command("update", translate.updateDescription())
-    .scriptName(kleur.dim("• bolt"))
+    .command("init", locale["init"]["description"])
+    .command("lang", locale["lang"]["description"])
+    .command("run", locale["run"]["description"])
+    .command("update", locale["update"]["description"])
+    .scriptName("bolt")
     .version(false)
     .help(false)
-    .updateLocale({ "Commands:": translate.command() })
+    .updateLocale({ "Commands:": locale["cmd"]["command"] })
     .argv
 
-const command = yargs.argv._[0]
-if (command == "lang") {
-    lang()
-} else if (command == "update") {
-    update()
-} else if (command == "init") {
-    init()
-} else if (command == "run") {
-    run()
-} else yargs.showHelp()
+switch (yargs.argv._[0]) {
+    case "init":
+        init()
+        break
+    case "lang":
+        lang()
+        break
+    case "run":
+        run()
+        break
+    case "update":
+        update()
+        break
+    default:
+        yargs.showHelp()
+}
